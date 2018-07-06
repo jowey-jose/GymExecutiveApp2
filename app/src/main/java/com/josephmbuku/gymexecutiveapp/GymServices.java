336a91c08aa5a0cb2d152d852cd24429f97cb78c
jowey-jose <josephmbuku17@gmail.com>
2018-07-06 10:05:34 +0300

    Initial commit

---
+++ app/src/main/java/com/josephmbuku/gymexecutiveapp/GymServices.java
@@ -0,0 +1,13 @@
+package com.josephmbuku.gymexecutiveapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GymServices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gym_services);
+    }
+}
